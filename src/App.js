a4cac7b2bd981bc106b48834935c9350efecb06b
David <david.nweke@outlook.com>
2022-03-10 12:15:36 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { Box, Container, Flex } from "@chakra-ui/layout";
+import "./App.css";
+import Main from "./components/main/main";
+import SidBar from "./components/sidBar/SidBar";
+import SideProfile from "./components/sideProfile/sideProfile";
+
+const App = () => {
+  return (
+    <Container bg="#f8fafd" maxW="Container.3xl" height="100vh" p="0">
+      <Flex height="full">
+        <Box
+          height="full"
+          flex={{
+            base: 0.5,
+            sm: 0.5,
+            md: 0.5,
+            lg: 0.5,
+            xl: 1,
+          }}
+          display={["none","block","block","block","block"]}
+          mr={["30px", "20px", "30px", "60px", "50px"]}
+        >
+          <SidBar />
+        </Box>
+        <Box height="full" flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+          <Main />
+        </Box>
+        <Box
+          bg="white"
+          height="full"
+          flex={2}
+          display={["none", "none", "block", "block", "block"]}
+        >
+          <SideProfile />
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/components/sidBar/SidBar.js
@@ -0,0 +1,196 @@
+import React from "react";
+
+import { Flex, Heading, Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { BsFillFolderFill } from "react-icons/bs";
+import { IoMdAnalytics } from "react-icons/io";
+import { IoCalendarSharp, IoSettings } from "react-icons/io5";
+import { HiHome } from "react-icons/hi";
+
+const SidBar = () => {
+  return (
+    <Flex
+    pr={["0px", "0px", "0px","0px","40px"]}
+      position="fixed"
+      height="full"
+      bg="white"
+      flexDirection="column"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Flex pl="4" pt="4" justifyContent="flex-start" flexDirection="column">
+        <Heading
+          fontSize="2xl"
+          color="gray.700"
+          mb="10"
+          display={{
+            base: "none",
+            sm: "none",
+            md: "none",
+            lg: "none",
+            xl: "block",
+          }}
+        >
+          Navigations
+        </Heading>
+
+        <Flex
+          cursor="pointer"
+          alignItems="center"
+          width="90%"
+          bg="gray.100"
+          height="33px"
+          borderRadius="lg"
+        >
+          <Button
+            leftIcon={<HiHome />}
+            _hover={{}}
+            _focus={{}}
+            _active={{}}
+            bg="transparent"
+            fontSize="md"
+            color="gray.700"
+          >
+            <Text
+              display={{
+                base: "none",
+                sm: "none",
+                md: "none",
+                lg: "none",
+                xl: "block",
+              }}
+            >
+              Home
+            </Text>
+          </Button>
+        </Flex>
+
+        <Flex
+          cursor="pointer"
+          alignItems="center"
+          width="90%"
+          height="33px"
+          mt="3"
+          borderRadius="lg"
+        >
+          <Button
+            leftIcon={<BsFillFolderFill />}
+            _hover={{}}
+            _focus={{}}
+            _active={{}}
+            bg="transparent"
+            fontSize="md"
+            color="gray.700"
+          >
+            <Text
+              display={{
+                base: "none",
+                sm: "none",
+                md: "none",
+                lg: "none",
+                xl: "block",
+              }}
+            >
+              Folder
+            </Text>
+          </Button>
+        </Flex>
+
+        <Flex
+          mt="3"
+          cursor="pointer"
+          alignItems="center"
+          width="90%"
+          height="33px"
+          borderRadius="lg"
+        >
+          <Button
+            leftIcon={<IoMdAnalytics />}
+            _hover={{}}
+            _focus={{}}
+            _active={{}}
+            bg="transparent"
+            fontSize="md"
+            color="gray.700"
+          >
+            <Text
+              display={{
+                base: "none",
+                sm: "none",
+                md: "none",
+                lg: "none",
+                xl: "block",
+              }}
+            >
+              Analytics
+            </Text>
+          </Button>
+        </Flex>
+
+        <Flex
+          mt="3"
+          cursor="pointer"
+          alignItems="center"
+          width="90%"
+          height="33px"
+          borderRadius="lg"
+        >
+          <Button
+            leftIcon={<IoCalendarSharp />}
+            _hover={{}}
+            _focus={{}}
+            _active={{}}
+            bg="transparent"
+            fontSize="md"
+            color="gray.700"
+          >
+            <Text
+              display={{
+                base: "none",
+                sm: "none",
+                md: "none",
+                lg: "none",
+                xl: "block",
+              }}
+            >
+              Calendar
+            </Text>
+          </Button>
+        </Flex>
+
+        <Flex
+          mt="3"
+          cursor="pointer"
+          alignItems="center"
+          width="90%"
+          height="33px"
+          borderRadius="lg"
+        >
+          <Button
+            leftIcon={<IoSettings />}
+            _hover={{}}
+            _focus={{}}
+            _active={{}}
+            bg="transparent"
+            fontSize="md"
+            color="gray.700"
+          >
+            <Text
+              display={{
+                base: "none",
+                sm: "none",
+                md: "none",
+                lg: "none",
+                xl: "block",
+              }}
+            >
+              Setting
+            </Text>
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default SidBar;
